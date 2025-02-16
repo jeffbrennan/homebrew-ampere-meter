ddfac2990846621c21af97ac8e4b1acad9aedc38
Jeff Brennan <jeffbrennan10@gmail.com>
2025-02-16 16:00:48 -0500

    update url, remove trailing whitespace

--- Formula/ampere-meter.rb
+++ Formula/ampere-meter.rb
@@ -3,9 +3,9 @@ class AmpereMeter < Formula
 
   desc "CLI tool to track github organization metrics"
   homepage "https://github.com/jeffbrennan/ampere"
-  url "https://files.pythonhosted.org/packages/28/61/86adcf867061f0690e3e86ad2fbbbe392815319043d44a07d5328adf0a88/ampere_meter-0.1.2.tar.gz"
+  url "https://github.com/jeffbrennan/ampere/releases/download/0.1.2/ampere_meter-0.1.2.tar.gz"
   sha256 "95a5578dc46636e81b263f233c7789697c6e9c9c662977b26542207b81dbe056"
-  
+
   depends_on "rust" => :build
   depends_on "python@3.11"
 
